8dcacc7e7f35782e210a6a3517ddb17792dee6e5
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-08-09 14:20:38 +0500

    font universal qilib quyildi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 
 export default function Home() {
   return (
-   <>
-   </>
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Taxing Laughter: The Joke Tax Chronicles
+    </h1>
   );
 }
