b83802ec238b674d1bd8d78272ed3c6a74e2944e
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-08-09 16:22:30 +0500

    sardorbek

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <>
-      <div className="w-40 h-40 bg-black"></div>
+      <div className="w-40 h-40 bg-black">
+         
+      </div>
     </>
   );
 }
