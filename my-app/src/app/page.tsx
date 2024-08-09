963a0af455566d9ce3bcbb2c801f71be4303c7c8
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-08-09 19:01:31 +0500

    boldi uje

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,10 @@
+import Images from "@/assets/ImagesConst";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-52 h-52 bg-black">
-      dasdasdasd
+    <main className="">
+      <Image src={Images.Logo} className="" alt="Next.js Logo" width={180} height={37} priority />
     </main>
   );
 }
---
+++ my-app/src/assets/ImagesConst.tsx
@@ -0,0 +1,5 @@
+import Logo from './images/svg/Layer_1 (1).svg'
+const Images={
+    Logo,
+}
+export default Images;
