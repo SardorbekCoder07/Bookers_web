a41256bc52ae6fc630156f3c4943e8d7bf226bdd
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-08-10 09:33:23 +0500

    haa

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,40 +1,9 @@
 import Images from "@/assets/ImagesConst";
 import Image from "next/image";
-import {
-  AlertDialog,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogFooter,
-  AlertDialogTitle,
-  AlertDialogDescription,
-  AlertDialogAction,
-  AlertDialogCancel,
-} from "@/components/ui/alert-dialog"
-import { Button } from "@/components/ui/button"
-
 export default function Home() {
   return (
     <main className="">
       <Image src={Images.Logo} className="" alt="Next.js Logo" width={180} height={37} priority />
-      <AlertDialog>
-        <AlertDialogTrigger>
-          <Button variant="outline">Show Dialog</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
     </main>
   );
 }
