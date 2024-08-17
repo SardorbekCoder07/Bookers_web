1ba420521a2a849518adb361920e761781a66c3e
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-08-17 15:49:14 +0500

    уйц

--- src/app/bookersBusiness/page.tsx
+++ src/app/bookersBusiness/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+"use client";
+
+import BookersBussines from "@/Components/cards/BookersBussines";
+import Hero from "@/Components/Hero/page";
+import HeaderTitles from "@/Components/text/HeaderBookers";
+import Subtitle from "@/Components/text/subtitle";
+
 
 const BookersBiznes = () => {
-  return (
-    <div>
-dddddddd
-    </div>
-  )
-}
 
-export default BookersBiznes
+  return (
+    <div className=" container">
+      <Hero title={'fewfew'} />
+      <div className="w-full md:w-[70%] mb-5">
+        <HeaderTitles text={''} />
+      </div>
+      <div className="w-full md:w-[65%] mb-5">
+        <Subtitle text={''} />
+      </div>
+      <div className="w-full md:w-[60%] mb-10">
+        <Subtitle text={''} />
+      </div>
+      <div className="mb-10 w-full">
+        <BookersBussines />
+      </div>
+    </div>
+  );
+};
+
+export default BookersBiznes;
