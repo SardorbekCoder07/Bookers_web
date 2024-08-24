c1378bd03115cde1bc9ff306bdcfc3f705a5a809
abdulvohid31 <abdulvohidcode@gmail.com>
2024-08-24 12:37:07 +0500

    rfddf

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ import { BASE_URL } from './Urls';
 axios.defaults.baseURL = BASE_URL;
 
 axios.interceptors.request.use((config) => {
-  const token = '';
+  const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4NTA4ODkwMDM3In0.PDAUgTvdTY39g0j2BMGxe5rMhjEOcIGKfRQIHzT78XJI-mZ9Dzhd7_1j2EBwM1U5eEg6--X6q9JH3BDooiYnoA';
   config.headers.Authorization = token ? `Bearer ${token}` : '';
   return config;
 });
