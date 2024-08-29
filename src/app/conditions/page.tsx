71cfdafe7fd371770dc598b2c9d0614058a4d4d7
omadbekolimov <omadbekolimov96@gmail.com>
2024-08-29 14:19:11 +0500

    gooo usloviya

---
+++ src/app/conditions/page.tsx
@@ -0,0 +1,18 @@
+import PricingTable from '@/Components/Tarif/Tarif';
+import HeaderTitles from '@/Components/text/HeaderBookers';
+import React from 'react';
+
+const Tarif = () => {
+    return (
+        <div className='mb-10 mt-40'>
+            <div className='mt-20 w-full md:w-[60%] mb-4 mx-auto '>
+                <HeaderTitles
+                    text='Условия использования'
+                />
+            </div>
+              
+        </div>
+    );
+}
+
+export default Tarif;
