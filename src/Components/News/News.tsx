0437b87c237e2bf908c165ad689b3dae206770a4
abdulvohid31 <abdulvohidcode@gmail.com>
2024-08-30 17:53:18 +0500

    casd

--- src/Components/News/News.tsx
+++ src/Components/News/News.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Pagination } from 'swiper/modules';
+import { MdDateRange } from 'react-icons/md';
+import { Autoplay, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const newsData = [
@@ -24,18 +25,43 @@ const newsData = [
         description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since...',
         imageUrl: 'path_to_image3.jpg',
     },
+    {
+        id: 3,
+        date: '18 июня 2024 г.',
+        title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since...',
+        imageUrl: 'path_to_image3.jpg',
+    },
+    {
+        id: 3,
+        date: '18 июня 2024 г.',
+        title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since...',
+        imageUrl: 'path_to_image3.jpg',
+    },
+    {
+        id: 3,
+        date: '18 июня 2024 г.',
+        title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since...',
+        imageUrl: 'path_to_image3.jpg',
+    },
 ];
 
 export default function News() {
     return (
-        <div className="bg-gray-900 py-10">
+        <div className="py-10">
             <Swiper
                 slidesPerView={3}
                 spaceBetween={30}
                 pagination={{
                     clickable: true,
                 }}
-                modules={[Pagination]}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: true,
+                }}
+                modules={[Autoplay]}
                 className="mySwiper"
             >
                 {newsData.map(news => (
@@ -44,9 +70,7 @@ export default function News() {
                             <img src={news.imageUrl} alt={news.title} className="w-full h-48 object-cover" />
                             <div className="p-6">
                                 <p className="text-red-400 text-sm mb-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-5 h-5 mr-2">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8 7h4m-2 0v10m4 0h4M6 3v1M3 6h1M21 6h1M6 21v1M3 18h1M21 18h1M18 3v1m-3 15h4m-2 0V7M4 7h4m-2 0v10m4 0h4m-2 0V7m4 0h4m-2 0v10" />
-                                    </svg>
+                                    <MdDateRange/>
                                     {news.date}
                                 </p>
                                 <h3 className="text-lg font-semibold text-white mb-4">{news.title}</h3>
